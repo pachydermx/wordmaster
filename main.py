be1a876872f08409057a05a300a9484f975bc32e
Ren Jimin <renjimin@gmail.com>
2016-12-13 01:47:36 +0900

    Loader

--- main.py
+++ main.py
@@ -3,24 +3,44 @@ import json
 import io
 
 class WordMaster(Tkinter.Tk):
-    data = None
+
     def __init__(self, parent):
+        # var
+        self.id = 0
+        # gui
         Tkinter.Tk.__init__(self, parent)
         self.parent = parent
+        self.loadWords()
         self.initialize()
 
     def initialize(self):
         self.grid()
 
-        self.entry = Tkinter.Entry(self)
-        self.entry.grid(column = 0, row = 0, sticky = "EW")
-        self.entry.bind("a", self.OnPressEnter)
 
-        button = Tkinter.Button(self, text="Click me", command=self.OnButtonClick)
+        #self.entry = Tkinter.Entry(self)
+        #self.entry.grid(column = 0, row = 0, sticky = "EW")
+        self.bind("a", self.OnPressEnter)
+        self.bind("]", self.onPressNext)
+        self.bind("[", self.onPressPrev)
+
+        button = Tkinter.Button(self, text="BTN", command=self.OnButtonClick)
         button.grid(column = 1, row = 0)
 
-        label = Tkinter.Label(self, anchor="w", fg="white", bg="blue")
-        label.grid(column = 0, row = 1, columnspan=2, sticky = "EW")
+        self.idLabelVar = Tkinter.StringVar()
+        self.idLabel = Tkinter.Label(self, textvariable=self.idLabelVar, anchor="w")
+        self.idLabel.grid(column = 0, row = 2, columnspan = 2, sticky = "W")
+
+        self.wordLabelVar = Tkinter.StringVar()
+        self.wordLabel = Tkinter.Label(self, textvariable=self.wordLabelVar, anchor="w")
+        self.wordLabel.grid(column = 0, row = 3, columnspan = 2, sticky = "W")
+
+        self.equLabelVar = Tkinter.StringVar()
+        self.equLabel = Tkinter.Label(self, textvariable=self.equLabelVar, anchor="w")
+        self.equLabel.grid(column = 0, row = 4, columnspan = 2, sticky = "W")
+
+        self.desLabelVar = Tkinter.StringVar()
+        self.desLabel = Tkinter.Label(self, textvariable=self.desLabelVar, anchor="w")
+        self.desLabel.grid(column = 0, row = 5, columnspan = 2, sticky = "W")
 
         self.grid_columnconfigure(0, weight = 1)
 
@@ -28,12 +48,28 @@ class WordMaster(Tkinter.Tk):
         print "you clicked me"
 
     def OnPressEnter(self, event):
-        print "you pressed enter"
+        self.idLabelVar.set("loaded")
+
+    def onPressPrev(self, event):
+        self.id -= 1
+        self.showWord()
+
+    def onPressNext(self, event):
+        self.id += 1
+        self.showWord()
+
+    def showWord(self):
+        self.idLabelVar.set(self.data[self.id][0])
+        self.wordLabelVar.set(self.data[self.id][1])
+        self.equLabelVar.set(self.data[self.id][2])
+        self.desLabelVar.set(self.data[self.id][3])
+
+    def loadWords(self):
+        with open('data.txt') as data_file:
+            self.data = json.load(data_file)
+        print self.data
 
 if __name__ == "__main__":
     wm = WordMaster(None)
     wm.title = "WordMaster"
     wm.mainloop()
-    data = [["a", "equ", "des"], ["b", "equb", "desb"]]
-    with io.open("data.txt", "w", encoding="utf-8") as f:
-        f.write(unicode(json.dumps(data, ensure_ascii=False)))
--- wordloader.py
+++ wordloader.py
@@ -8,9 +8,21 @@ with io.open("wordutf.txt", "r", encoding="UTF-8") as inputFile:
     for line in inputFile:
         element = line.split(" ")
         if len(element) > 3:
-            print line
-            print element
-            result.append(element)
+            stepTwo = line.split("=")
+            if len(stepTwo) > 1:
+                leftSide = stepTwo[0].split(".")
+                print leftSide[1]
+                id = leftSide[0]
+                word = leftSide[1]
+                rightSide = stepTwo[1].split(".")
+                equ = rightSide[0]
+                desS = stepTwo[1].split(" ")
+                des = desS[-1]
+                result.append([id, word, equ, des])
+            else :
+                print line
+                print element
+                result.append(element)
 
 
 with io.open("data.txt", "w", encoding="utf-8") as f:
