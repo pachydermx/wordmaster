ceacffacf6a052e204a83aa475e212fb4fa8be8a
PachyLabPC <renjimin@gmail.com>
2016-12-15 17:34:51 +0900

    Stat

---
+++ stat.py
@@ -0,0 +1,29 @@
+import json, io
+import datetime
+
+class Stat:
+    def __init__(self):
+        self.load()
+
+    def load(self):
+        with open('misc.txt') as data_file:
+            self.data = json.load(data_file)
+
+    def save(self):
+        with io.open("misc.txt", "w", encoding="utf-8") as f:
+            f.write(unicode(json.dumps(self.data, ensure_ascii=False)))
+
+    def getDateCount(self):
+        dateStamp = datetime.date.today().isoformat()
+        if dateStamp in self.data["counter"]:
+            return self.data["counter"][dateStamp]
+        else:
+            return 0
+
+    def dateCount(self):
+        dateStamp = datetime.date.today().isoformat()
+        if dateStamp in self.data["counter"]:
+            self.data['counter'][dateStamp] = int(self.data['counter'][dateStamp]) + 1
+        else:
+            self.data['counter'][dateStamp] = 1
+        
